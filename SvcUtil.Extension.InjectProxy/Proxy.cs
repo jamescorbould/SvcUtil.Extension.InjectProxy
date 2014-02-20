38a8003eae58ede9f01ef424c27b3a29eddd7db4
jamescorbould <james.bron@hotmail.com>
2014-02-21 07:41:30 +1300

    Path to config file now dynamic.

--- SvcUtil.Extension.InjectProxy/Proxy.cs
+++ SvcUtil.Extension.InjectProxy/Proxy.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
 using System.Configuration;
+using System.Reflection;
+using System.Linq;
 
 namespace SvcUtil.Extension.InjectProxy
 {
@@ -13,6 +15,10 @@ namespace SvcUtil.Extension.InjectProxy
 
         public Proxy ()
         {
+            // Change the AppDomain default config file location, since this assembly will be installed in the GAC.
+            AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", ConfigurationManager.AppSettings["pathToConfigFile"]);
+            ResetConfigMechanism();
+
             this.domain = ConfigurationManager.AppSettings["domain"];
             this.username = ConfigurationManager.AppSettings["username"];
             this.password = ConfigurationManager.AppSettings["password"];
@@ -33,6 +39,29 @@ namespace SvcUtil.Extension.InjectProxy
         public bool IsBypassed(Uri host)
         {
             return false;
+        }
+
+        // Taken from http://stackoverflow.com/questions/6150644/change-default-app-config-at-runtime/6151688#6151688
+        private static void ResetConfigMechanism()
+        {
+            typeof(ConfigurationManager)
+                .GetField("s_initState", BindingFlags.NonPublic |
+                                         BindingFlags.Static)
+                .SetValue(null, 0);
+
+            typeof(ConfigurationManager)
+                .GetField("s_configSystem", BindingFlags.NonPublic |
+                                            BindingFlags.Static)
+                .SetValue(null, null);
+
+            typeof(ConfigurationManager)
+                .Assembly.GetTypes()
+                .Where(x => x.FullName ==
+                            "System.Configuration.ClientConfigPaths")
+                .First()
+                .GetField("s_current", BindingFlags.NonPublic |
+                                       BindingFlags.Static)
+                .SetValue(null, null);
         }
     }
 }
